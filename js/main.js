8b66de9179f91db7692ef16f1ea6b33c8cb9efeb
MATVEN <mattiavena2993@gmail.com>
2019-10-18 01:09:37 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
-// ask age
-var age = prompt("How old are you?");
-console.log(age);
-// ask km distance
-var km = prompt("How many km do you have to do?");
-console.log(km)
+// var
+var age = parseInt(prompt("How old are you?"));
+var km = parseInt(prompt("How many km do you have to do?"));
+var price = Math.round(0.21 * km);
+var sale;
+// html
+var boxsconto = document.getElementById('scontato');
+var boxintero = document.getElementById('intero');
+var boxtot = document.getElementById('totale');
+
 // calculate ticket price
-// young price
 // old price
+if(age>=65) {
+  boxintero.innerHTML = price + "€";
+  sale = Math.round(price * 0.4);
+  boxsconto.innerHTML = sale + "€ sconto anziani ";
+  boxtot.innerHTML = price - sale + "€";
+
+  // young price
+} else if (age<=17) {
+  boxintero.innerHTML = price + "€";
+  sale = Math.round(price * 0.2);
+  boxsconto.innerHTML = sale + "€ sconto giovani ";
+  boxtot.innerHTML = price - sale + "€";
+
+  // normal price
+} else {
+  boxintero.innerHTML = price + "€";
+  boxsconto.innerHTML = "0€";
+  boxtot.innerHTML = price + "€";
+}
