0ea4a1f31b9eac4933efb65c737f4e44935a1f21
MATVEN <mattiavena2993@gmail.com>
2019-10-17 16:04:47 +0200

    questions

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// ask age
+var age = prompt("How old are you?");
+console.log(age);
+// ask km distance
+var km = prompt("How many km do you have to do?");
+console.log(km)
+// calculate ticket price
+// young price
+// old price
